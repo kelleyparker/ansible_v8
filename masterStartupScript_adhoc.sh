5d2b0aadfcfc263452982301f1a2d9e88f30ec18
Kelley Parker <kelleyparker@users.noreply.github.com>
2022-01-20 01:13:57 -0600

    Create masterStartupScript_adhoc.sh

---
+++ masterStartupScript_adhoc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ansible all -m shell -a "dnf upgrade -y"                                # Update kernel, packages, etc.
+ansible all -m service -a "name=httpd state=started enabled=yes"        # Install httpd, start it, enable it.
+ansible all -m user -a 'name=cliffgoat password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=ansible password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=martha password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=jack password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=jill password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=jackson password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=megaman password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=megamanx password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=stingchameleon password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=sparkmandrill password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=stormeagle password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=flamemammoth password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=chillpenguin password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=blizzardbuffalo password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=sigma password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=vile password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=sting password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
+ansible all -m user -a 'name=DarbyAllin password="$6$v.xLtM75wuxk.3gR$X4fCrASF8StnUoq7o5OcfKvs0.KUxD4N2CzdNKkgqaqKlQxIut/ngne0UCDiWZAFhAfkkQwZN8Q8w5NdEHHfy0"'
